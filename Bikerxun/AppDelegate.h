141f97da470f33ba98ea484dae9f739d46d2e38a
Bobie <bobie@herxun.co>
2013-03-13 15:57:13 +0800

    Initial Commit

---
+++ Bikerxun/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Bikerxun
+//
+//  Created by Bobie on 3/13/13.
+//  Copyright (c) 2013 Bobie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Bikerxun/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Bikerxun
+//
+//  Created by Bobie on 3/13/13.
+//  Copyright (c) 2013 Bobie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
